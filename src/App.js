0a5ac6455547231e46d03b4b96462939380092c8
haylin <eileenza@protonmail.com>
2023-02-28 21:03:01 -0500

    final commit, I will become a React pro

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/h04032013"
           target="_blank"
           rel="noopener noreferrer"
         >
