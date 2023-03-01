6aed345d3a82aca650802ad7e85fc9b62437c2a4
haylin <eileenza@protonmail.com>
2023-02-28 21:10:24 -0500

    forgot to say 'check me out'...final commit for real!!

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me Out!
         </a>
       </header>
     </div>
